4220a9afcc0d72918342cf9288515e49426d9eed
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-11-25 21:34:49 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -12,7 +12,7 @@ function forLoop(array) {
 function whileLoop(n) {
   let countdown = n;
 
-    while (countdown > 0) {
+    while (countdown > 10) {
       console.log(--countdown)
     }
     return "done";
