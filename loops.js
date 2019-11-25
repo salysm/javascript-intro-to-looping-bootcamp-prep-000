16dd0422b906f465779467a1a0ccb919ffb066e3
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-11-25 20:39:42 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -21,10 +21,4 @@ function whileLoop(n) {
 function doWhileLoop(array) {
   function maybeTrue() {
     return Math.random() >= 0.5
-  }
-
-  do {
-    array.pop()
-  } while (array.length > -1 && maybeTrue());
-    return array;
-}
+  }}
