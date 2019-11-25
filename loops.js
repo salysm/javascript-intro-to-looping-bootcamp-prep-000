15019a5bac2a24acc308520e1d06dccc893bebbd
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-11-25 22:14:37 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,22 +1,17 @@
+
 function forLoop(array) {
-   for ( let i = 0; i < 25; i++) {
-     if (i === 1) {
-       array.push(`I am 1 strange loop.`)
-     } else {
-       array.push(`I am ${i} strange loops.`)
-     }
+  for (let i = 0; i < 25 ; i++) {
+    array.push('I am ${i} strange loop ${i===0 ? \'\' : \'s\' }.')
   }
   return array;
 }
 
 function whileLoop(n) {
-  let countdown = n;
-
-    while (countdown > 0) {
-      console.log(--countdown)
-    }
-    return "done";
+  while (n > 0) {
+    console.log(--n);
   }
+  return 'done'
+}
 
 function maybeTrue() {
   return Math.random() >= 0.5
@@ -24,7 +19,7 @@ function maybeTrue() {
 
 function doWhileLoop(array) {
   do {
-    array = array(1)
-  } while (array.length > 0 && maybeTrue())
-  return array
+    array.pop();
+    return array;
+  } while ( array.length > 0 && maybeTrue() );
 }
